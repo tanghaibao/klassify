190d6b4f06da195121bd5ada6b447233a29fc203
Haibao Tang <tanghaibao@gmail.com>
2024-07-18 21:32:02 +0800

    remove scaling_factors (#9)
    * remove scaling_factors
    
    * use file prefix

--- src/build.rs
+++ src/build.rs
@@ -1,4 +1,4 @@
-use crate::models::SingletonKmers;
+use crate::models::{prefix, SingletonKmers};
 use bincode::serialize_into;
 use clap::Parser;
 use needletail::{parse_fastx_file, Sequence};
@@ -15,12 +15,16 @@ const KMER_SIZE: u8 = 24;
 pub struct BuildArgs {
     /// Input FASTA files
     pub fasta_files: Vec<String>,
+    /// K-mer size
+    #[clap(short, long, default_value_t = KMER_SIZE)]
+    pub kmer_size: u8,
 }
 
-pub fn build(fasta_files: &Vec<String>) {
+/// Convert FASTA files to singleton k-mers
+pub fn build(fasta_files: &Vec<String>, kmer_size: u8) {
     let all_sets = fasta_files
         .par_iter()
-        .map(|fasta_file| get_kmers(fasta_file))
+        .map(|fasta_file| get_kmers(fasta_file, kmer_size))
         .collect::<Vec<_>>();
     // Identify all the kmers that appear once and only once in all the files
     let mut kmer_counts = HashMap::new();
@@ -52,18 +56,12 @@ pub fn build(fasta_files: &Vec<String>) {
             singleton_kmers_per_file
         })
         .collect::<Vec<_>>();
+    let fasta_files = fasta_files.iter().map(|x| prefix(x)).collect::<Vec<_>>();
     // Serialize the singleton kmers to a file
-    let counts = singletons
-        .iter()
-        .map(|x| x.len() as f64)
-        .collect::<Vec<_>>();
-    let max_count = counts.iter().cloned().fold(0. / 0., f64::max);
-    let scaling_factors = counts.iter().map(|&x| max_count / x).collect();
     let singleton_kmers = SingletonKmers {
-        kmer_size: KMER_SIZE,
-        fasta_files: fasta_files.clone(),
+        kmer_size,
+        fasta_files,
         kmers: singletons,
-        scaling_factors,
     };
     let output_file = "singleton_kmers.bc";
     let writer = BufWriter::new(File::create(output_file).unwrap());
@@ -71,13 +69,14 @@ pub fn build(fasta_files: &Vec<String>) {
     log::info!("Singleton kmers written to `{}`", output_file);
 }
 
-fn get_kmers(fasta_file: &str) -> HashSet<u64> {
+/// Get kmers from a FASTA file
+fn get_kmers(fasta_file: &str, kmer_size: u8) -> HashSet<u64> {
     let mut reader = parse_fastx_file(fasta_file).expect("valid FASTA file");
     let mut kmer_set = HashSet::new();
     while let Some(record) = reader.next() {
         let record = record.expect("valid record");
         let seq = record.normalize(false);
-        for (_, kmer, _) in seq.bit_kmers(KMER_SIZE, true) {
+        for (_, kmer, _) in seq.bit_kmers(kmer_size, true) {
             kmer_set.insert(kmer.0);
         }
     }
