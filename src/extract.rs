0ca2158e6a7d8f72f327b02589a8e3d9af0d1444
Haibao Tang <tanghaibao@gmail.com>
2024-07-20 18:55:34 +0800

    Dev (#20)
    * combine extracted FASTA
    
    * cleanup README

--- src/extract.rs
+++ src/extract.rs
@@ -14,6 +14,9 @@ pub struct ExtractArgs {
     pub reads_tsv: String,
     /// FASTA files to extract kmers
     pub fasta_files: Vec<String>,
+    /// Output file
+    #[clap(short, long, default_value = "extracted.fasta")]
+    pub output_file: String,
 }
 
 fn get_read_ids(reads_tsv: &str) -> HashMap<String, String> {
@@ -36,7 +39,7 @@ fn get_read_ids(reads_tsv: &str) -> HashMap<String, String> {
     read_map
 }
 
-fn extract_one(read_map: &HashMap<String, String>, fasta_file: &str) {
+fn extract_one(read_map: &HashMap<String, String>, fasta_file: &str) -> String {
     let mut reader = parse_fastx_file(fasta_file).expect("valid reads file");
     let file_prefix = prefix(fasta_file);
     let output_file = file_prefix.to_string() + ".extracted.fasta";
@@ -63,11 +66,25 @@ fn extract_one(read_map: &HashMap<String, String>, fasta_file: &str) {
         }
     }
     log::info!("Extracted {} reads from `{}`", total, fasta_file);
+    output_file
 }
 
-pub fn extract(reads_tsv: &str, fasta_files: &Vec<String>) {
+/// Extract reads from FASTA/FASTQ files
+pub fn extract(reads_tsv: &str, fasta_files: &Vec<String>, output_file: &str) {
     let read_map = get_read_ids(reads_tsv);
-    fasta_files.par_iter().for_each(|fasta_file| {
-        extract_one(&read_map, fasta_file);
-    });
+    let output_files = fasta_files
+        .par_iter()
+        .map(|fasta_file| extract_one(&read_map, fasta_file))
+        .collect::<Vec<_>>();
+    // Merge the output files
+    let mut writer = std::fs::File::create(output_file).unwrap();
+    for output_file in output_files.iter() {
+        let mut reader = std::fs::File::open(&output_file).unwrap();
+        std::io::copy(&mut reader, &mut writer).expect("valid copy");
+    }
+    // Cleanup the output files
+    for output_file in output_files {
+        std::fs::remove_file(output_file).expect("valid remove");
+    }
+    log::info!("Extracted reads written to `{}`", output_file);
 }
