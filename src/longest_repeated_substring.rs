5391d866a8af9428edf974207cc91b01a5b45e3b
Haibao Tang <tanghaibao@gmail.com>
2024-08-10 19:38:50 -0700

    update default min_length

--- src/longest_repeated_substring.rs
+++ src/longest_repeated_substring.rs
@@ -10,7 +10,7 @@ pub struct LongestRepeatedSubstringArgs {
     /// FASTA file to extract reads
     pub fasta_file: String,
     /// Minimum length of the repeated substring
-    #[clap(short, long, default_value_t = DEFAULT_MIN_REPEAT_LENGTH)]
+    #[clap(short, long, default_value_t = 30000)]
     pub min_length: isize,
 }
 
@@ -31,14 +31,17 @@ pub fn longest_repeated_substring(fasta_file: &str, min_length: isize) {
     log::info!("Read {} sequences from FASTA file", seq_starts.len());
 
     let sa = suffix_array(&text);
+    log::info!("Computed suffix array: {}", sa.len());
     let lcp = lcp(&text, &sa);
+    log::info!("Computed LCP array: {}", lcp.len());
     for (index, lc) in lcp.iter().enumerate() {
         if lc >= min_length {
             // let seq_index = seq_starts.binary_search(&sa[index]).unwrap();
             let text_index = sa[index];
             let next_text_index = sa[index + 1];
             println!(
-                "{}… {}…",
+                "lcp={} => {}… {}…",
+                lc,
                 String::from_utf8(text[text_index..text_index + 20].to_vec()).unwrap(),
                 String::from_utf8(text[next_text_index..next_text_index + 20].to_vec()).unwrap()
             );
