1761b1605effa26cc274bb14191e54d6f7ac0876
Haibao Tang <tanghaibao@gmail.com>
2024-08-12 22:37:47 -0700

    print range

--- src/longest_repeat.rs
+++ src/longest_repeat.rs
@@ -4,7 +4,6 @@ use bio::data_structures::suffix_array::{lcp, suffix_array};
 use clap::Parser;
 use log;
 use needletail::{parse_fastx_file, Sequence};
-use std::collections::HashMap;
 
 const TERMINATOR: u8 = b'$';
 
@@ -17,55 +16,104 @@ pub struct LongestRepeatArgs {
     pub min_length: isize,
 }
 
-/// Mark the start and stop for a sequence within the concatenated text
-struct Seq {
+/// A range of positions in a sequence
+struct SeqRange {
+    name: String,
     start: usize,
-    size: usize,
+    end: usize,
 }
 
 /// Container for multiple sequences, concatenated together
 struct MultiSequence {
     text: Vec<u8>,
-    seqs: HashMap<String, Seq>,
+    names: Vec<String>,
+    starts: Vec<usize>,
+}
+
+impl MultiSequence {
+    /// Get the sequence and its start and end positions in the concatenated text
+    fn slice(&self, start: usize, end: usize) -> Option<SeqRange> {
+        let start_index = self.bisect_left(start);
+        let end_index = self.bisect_left(end);
+        if start_index != end_index {
+            return None;
+        }
+        let name = self.names[start_index].clone();
+        let seq_start = self.starts[start_index];
+        let new_start = start - seq_start;
+        let mut new_end = end - seq_start;
+        if self.text[new_end] == TERMINATOR {
+            new_end -= 1;
+        }
+        Some(SeqRange {
+            name,
+            start: new_start,
+            end: new_end,
+        })
+    }
+
+    /// Find index of the sequence containing the given position
+    #[inline]
+    fn bisect_left(&self, pos: usize) -> usize {
+        self.starts.binary_search(&pos).unwrap_or_else(|x| x - 1)
+    }
 }
 
 /// Find the longest repeated substring in a set of sequences
 pub fn longest_repeat(fasta_file: &str, min_length: isize) {
     let mut reader = parse_fastx_file(fasta_file).expect("valid reads file");
     let mut text = Vec::new();
-    let mut seqs = HashMap::new();
+    let mut names = Vec::new();
+    let mut starts = Vec::new();
     while let Some(record) = reader.next() {
         let record = record.expect("valid record");
         let seq = record.normalize(false);
         let name = String::from_utf8(record.id().to_vec()).unwrap();
-        seqs.insert(
-            name,
-            Seq {
-                start: text.len(),
-                size: seq.len(),
-            },
-        );
+        names.push(name);
+        starts.push(text.len());
         text.extend(seq.into_owned());
         text.push(TERMINATOR);
     }
-    let ms = MultiSequence { text, seqs };
-    log::info!("Read {} sequences from FASTA file", ms.seqs.len());
+    let ms = MultiSequence {
+        text,
+        names,
+        starts,
+    };
+    log::info!("Read {} sequences from FASTA file", ms.names.len());
 
     let sa = suffix_array(&ms.text);
     log::info!("Computed suffix array: {}", sa.len());
     let lcp = lcp(&ms.text, &sa);
     log::info!("Computed LCP array: {}", lcp.len());
     for (index, lc) in lcp.iter().enumerate() {
-        if lc >= min_length {
-            // let seq_index = seq_starts.binary_search(&sa[index]).unwrap();
-            let text_index = sa[index];
-            let next_text_index = sa[index + 1];
-            println!(
-                "lcp={} => {}… {}…",
-                lc,
-                String::from_utf8(ms.text[text_index..text_index + 20].to_vec()).unwrap(),
-                String::from_utf8(ms.text[next_text_index..next_text_index + 20].to_vec()).unwrap()
-            );
+        if lc < min_length {
+            continue;
         }
+        let start = sa[index];
+        let end = start + lc as usize;
+        let next_start = sa[index + 1];
+        let next_end = next_start + lc as usize;
+        let seqrange = ms.slice(start, end);
+        let next_seqrange = ms.slice(next_start, next_end);
+        if seqrange.is_none() || next_seqrange.is_none() {
+            continue;
+        }
+        let seqrange = seqrange.unwrap();
+        let next_seqrange = next_seqrange.unwrap();
+        println!(
+            "{}:{}-{} => {}:{}-{}",
+            seqrange.name,
+            seqrange.start,
+            seqrange.end,
+            next_seqrange.name,
+            next_seqrange.start,
+            next_seqrange.end
+        );
+        println!(
+            "lcp={} => {}… {}…",
+            lc,
+            String::from_utf8(ms.text[start..start + 20].to_vec()).unwrap(),
+            String::from_utf8(ms.text[next_start..next_start + 20].to_vec()).unwrap()
+        );
     }
 }
