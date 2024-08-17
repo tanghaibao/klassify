78c4713c6558873c320083fec067fbe7ff0257fc
Haibao Tang <tanghaibao@gmail.com>
2024-08-17 13:15:54 -0700

    add counter

--- scripts/split_reads_by_breakpoint.py
+++ scripts/split_reads_by_breakpoint.py
@@ -1,5 +1,7 @@
 import sys
 
+from collections import Counter
+from io import TextIOWrapper
 from typing import List
 
 import numpy as np
@@ -7,6 +9,9 @@ import numpy as np
 from jcvi.apps.base import OptionParser, logger
 from jcvi.formats.bed import Bed, BedLine
 from jcvi.formats.fasta import Fasta
+
+
+SUCCESS, FAIL = "SUCCESS", "FAIL"
 
 
 def main(args):
@@ -18,17 +23,24 @@ def main(args):
     bedfile, fastafile = args
     fasta = Fasta(fastafile)
     bed = Bed(bedfile)
-    for read, sb in bed.sub_beds():
-        get_breakpoint(read, sb, fasta)
+    new_fastafile = bedfile.replace(".gz", "").rsplit(".", 1)[0] + ".split.fasta"
+    counter = Counter()
+    with open(new_fastafile, "w", encoding="utf-8") as new_fasta:
+        for read, sb in bed.sub_beds():
+            ret = get_breakpoint(read, sb, fasta, new_fasta)
+            counter[ret] += 1
+    logger.info("Summary: %s", counter)
+    logger.info("Split fasta file written to `{}`".format(new_fastafile))
 
 
-def get_breakpoint(read: str, sb: List[BedLine], fasta: Fasta):
+def get_breakpoint(
+    read: str, sb: List[BedLine], fasta: Fasta, new_fasta: TextIOWrapper
+) -> str:
     """
     Given a read and its sub-beds, determine the breakpoint
     """
-    ra, rb, read_id = read.split("_", 2)
+    ra, rb, orig_read = read.split("_", 2)
     sids = [b.accn.split(":", 1)[0] for b in sb]
-
     n = len(sids)
 
     prefix_a = np.zeros(n, dtype="int")
@@ -58,13 +70,20 @@ def get_breakpoint(read: str, sb: List[BedLine], fasta: Fasta):
     print(sb[idx])
     if idx == n - 1:
         logger.info("Breakpoint at the end of `%s`. Skipped", read)
-        return
+        return FAIL
     right = sb[idx + 1].start
     print(sb[idx + 1])
     mid = (left + right) // 2
-    seq = fasta[read_id]
+    seq = fasta[read]
     end = len(seq)
-    print(mid, f"{ra}=0-{mid}", f"{rb}={mid}-{end}")
+    left_subread_id = f"{orig_read}|{ra}|0-{mid}"
+    left_subread = seq.seq[:mid]
+    right_subread_id = f"{orig_read}|{rb}|{mid}-{end}"
+    right_subread = seq.seq[mid:]
+    print(left_subread_id, right_subread_id)
+    new_fasta.write(f">{left_subread_id}\n{left_subread}\n")
+    new_fasta.write(f">{right_subread_id}\n{right_subread}\n")
+    return SUCCESS
 
 
 if __name__ == "__main__":
