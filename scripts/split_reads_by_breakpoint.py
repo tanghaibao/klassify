0f7e40e3d4766138608a5f8806efb76678247c43
Haibao Tang <tanghaibao@gmail.com>
2024-08-17 13:06:45 -0700

    also read in FASTA

--- scripts/split_reads_by_breakpoint.py
+++ scripts/split_reads_by_breakpoint.py
@@ -4,24 +4,29 @@ from typing import List
 
 import numpy as np
 
-from jcvi.apps.base import OptionParser
+from jcvi.apps.base import OptionParser, logger
 from jcvi.formats.bed import Bed, BedLine
+from jcvi.formats.fasta import Fasta
 
 
 def main(args):
     p = OptionParser(main.__doc__)
     _, args = p.parse_args(args)
+    if len(args) != 2:
+        sys.exit(not p.print_help())
 
-    bed = Bed(args[0])
+    bedfile, fastafile = args
+    fasta = Fasta(fastafile)
+    bed = Bed(bedfile)
     for read, sb in bed.sub_beds():
-        get_breakpoint(read, sb)
+        get_breakpoint(read, sb, fasta)
 
 
-def get_breakpoint(read: str, sb: List[BedLine]):
+def get_breakpoint(read: str, sb: List[BedLine], fasta: Fasta):
     """
     Given a read and its sub-beds, determine the breakpoint
     """
-    ra, rb = read.split("_", 2)[:2]
+    ra, rb, read_id = read.split("_", 2)
     sids = [b.accn.split(":", 1)[0] for b in sb]
 
     n = len(sids)
@@ -50,15 +55,16 @@ def get_breakpoint(read: str, sb: List[BedLine]):
         ra, rb = rb, ra
 
     left = sb[idx].end
-    if idx == n - 1:
-        logger.info(f"Read {read} is fully contained in {sb[idx]}")
-        mid = left
-    else:
-        right = sb[idx + 1].start
-        mid = (left + right) // 2
     print(sb[idx])
+    if idx == n - 1:
+        logger.info("Breakpoint at the end of `%s`. Skipped", read)
+        return
+    right = sb[idx + 1].start
     print(sb[idx + 1])
-    print(mid, f"{ra}=0-{mid}", f"{rb}={mid}-end")
+    mid = (left + right) // 2
+    seq = fasta[read_id]
+    end = len(seq)
+    print(mid, f"{ra}=0-{mid}", f"{rb}={mid}-{end}")
 
 
 if __name__ == "__main__":
