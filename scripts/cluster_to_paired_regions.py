cfecc42818442e4213a2f512fce21b43241eccb0
Haibao Tang <tanghaibao@gmail.com>
2024-08-17 21:04:10 -0700

    Dev (#33)
    * update notebook
    
    * Add scripts/split_reads_by_breakpoint.py
    
    * also read in FASTA
    
    * add counter
    
    * Add not enough kmers
    
    * Add cluster_to_paired_regions.py
    
    * update script
    
    * reorder regions
    
    * formatting
    
    * write paired poi
    
    * minor update
    
    * update regions file name
    
    * version 0.1.4

---
+++ scripts/cluster_to_paired_regions.py
@@ -0,0 +1,149 @@
+"""
+Cluster the reads into paired regions based on the alignment coordinates.
+"""
+
+import sys
+
+from collections import Counter, defaultdict
+from typing import List
+
+import numpy as np
+import networkx as nx
+
+from jcvi.formats.bed import Bed
+from jcvi.apps.base import OptionParser, logger
+
+
+SEQID_MISMATCH, SECONDARY_MATCH = "SEQID_MISMATCH", "SECONDARY_MATCH"
+MIN_READ_SUPPORT = 3
+
+
+def main(args: List[str]):
+    """
+    %prog bedfile
+    """
+    p = OptionParser(main.__doc__)
+    _, args = p.parse_args(args)
+    if len(args) != 1:
+        sys.exit(not p.print_help())
+
+    (bedfile,) = args
+    bed = Bed(bedfile)
+    filtered = []
+    seen = {}
+    counter = Counter()
+    # Store the primary match length
+    for b in bed:
+        length = b.end - b.start
+        seen[b.accn] = max(seen.get(b.accn, 0), length)
+    # Second pass to filter out the secondary matches
+    for b in bed:
+        expected_seqid = b.accn.split("|")[1]
+        if expected_seqid != b.seqid:
+            counter[SEQID_MISMATCH] += 1
+            continue
+        if seen[b.accn] != b.end - b.start:
+            counter[SECONDARY_MATCH] += 1
+            continue
+        filtered.append(b)
+    logger.info(counter)
+    logger.info("Total filtered: %d", len(filtered))
+
+    # Merge the regions that are clustered
+    merged = []
+    for b in filtered:
+        if merged and (
+            merged[-1][-1].seqid == b.seqid and merged[-1][-1].end >= b.start
+        ):
+            merged[-1].append(b)
+        else:
+            merged.append([b])
+
+    read_to_regions = defaultdict(list)
+    read_to_subreads = defaultdict(set)
+    read_to_match = {}
+    for mb in merged:
+        if len(mb) < MIN_READ_SUPPORT:
+            continue
+        mb_start = int(np.median([b.start for b in mb]))
+        mb_end = int(np.median([b.end for b in mb]))
+        region_name = f"{mb[0].seqid}:{mb_start}-{mb_end}"
+        for b in mb:
+            read_name = b.accn.split("|", 1)[0]
+            read_to_regions[read_name].append(region_name)
+            read_to_subreads[read_name].add((b.accn, b.strand))
+            read_to_match[b.accn] = f"{b.seqid}:{b.start}-{b.end}:{b.strand}"
+
+    pair_to_reads = defaultdict(list)
+    for read, regions in read_to_regions.items():
+        if len(regions) == 2:
+            pair_to_reads[tuple(sorted(regions))].append(read)
+
+    G = nx.Graph()
+    for pair, reads in pair_to_reads.items():
+        G.add_edge(*pair, weight=len(reads))
+    valid_pairs = sorted(tuple(sorted(x)) for x in nx.max_weight_matching(G))
+
+    filtered_pair_to_reads = {}
+    for pair in valid_pairs:
+        n_reads = len(pair_to_reads[pair])
+        if n_reads < MIN_READ_SUPPORT:
+            continue
+        counter = Counter()
+        filtered_reads = []
+        for read in pair_to_reads[pair]:
+            (fa, _), (fb, _) = read_to_subreads[read]
+            # Check if the subread is out of order
+            fa_read, fa_seqid, fa_range = fa.split("|")
+            fb_read, fb_seqid, fb_range = fb.split("|")
+            assert fa_read == fb_read
+            if fb_range.startswith("0-"):
+                assert not fa_range.endswith("0-")
+                fa, fb = fb, fa
+                fa_seqid, fb_seqid = fb_seqid, fa_seqid
+            counter[(fa_seqid, fb_seqid)] += 1
+            filtered_reads.append((fa, fb))
+        (ra_reordered, rb_reordered), _ = tuple(counter.most_common(1)[0])
+        ra, rb = pair
+        if not ra.startswith(ra_reordered):
+            ra, rb = rb, ra
+        assert ra.startswith(ra_reordered) and rb.startswith(rb_reordered)
+        filtered_pair_to_reads[(ra, rb)] = filtered_reads
+
+    # Finally, write out the paired regions
+    header = [
+        "Crossover ID",
+        "Left",
+        "Right",
+        "Read Count",
+        "Read ID",
+        "Read Left",
+        "Read Left Match",
+        "Read Right",
+        "Read Right Match",
+    ]
+    print("\t".join(header))
+    paired_pois = []
+    for cid, ((ra, rb), reads) in enumerate(sorted(filtered_pair_to_reads.items())):
+        cid += 1
+        paired_pois.append(ra)
+        paired_pois.append(rb)
+        for i, read in enumerate(reads):
+            row = [cid, "", "", ""] if i > 0 else [cid, ra, rb, len(reads)]
+            row += [
+                read[0].split("|")[0],
+                read[0].split("|")[2],
+                read_to_match[read[0]],
+                read[1].split("|")[2],
+                read_to_match[read[1]],
+            ]
+            print("\t".join(str(x) for x in row))
+
+    paired_regions_file = f"{bedfile.split('.', 1)[0]}.paired.regions"
+    with open(paired_regions_file, "w", encoding="utf-8") as fw:
+        fw.write("\n".join(paired_pois))
+    logger.info("Paired regions written to `%s`", paired_regions_file)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ scripts/split_reads_by_breakpoint.py
@@ -0,0 +1,100 @@
+import sys
+
+from collections import Counter
+from io import TextIOWrapper
+from typing import List
+
+import numpy as np
+
+from jcvi.apps.base import OptionParser, logger
+from jcvi.formats.bed import Bed, BedLine
+from jcvi.formats.fasta import Fasta
+
+
+SUCCESS, FAIL, NOT_ENOUGH_KMERS = "SUCCESS", "FAIL", "NOT_ENOUGH_KMERS"
+KMER_THRESHOLD = 30
+
+
+def main(args: List[str]):
+    p = OptionParser(main.__doc__)
+    _, args = p.parse_args(args)
+    if len(args) != 2:
+        sys.exit(not p.print_help())
+
+    bedfile, fastafile = args
+    fasta = Fasta(fastafile)
+    bed = Bed(bedfile)
+    new_fastafile = bedfile.replace(".gz", "").rsplit(".", 1)[0] + ".split.fasta"
+    counter = Counter()
+    with open(new_fastafile, "w", encoding="utf-8") as new_fasta:
+        for read, sb in bed.sub_beds():
+            ret = get_breakpoint(read, sb, fasta, new_fasta)
+            counter[ret] += 1
+    logger.info("Summary: %s", counter)
+    logger.info("Split fasta file written to `{}`".format(new_fastafile))
+
+
+def get_breakpoint(
+    read: str, sb: List[BedLine], fasta: Fasta, new_fasta: TextIOWrapper
+) -> str:
+    """
+    Given a read and its sub-beds, determine the breakpoint
+    """
+    ra, rb, orig_read = read.split("_", 2)
+    sids = [b.accn.split(":", 1)[0] for b in sb]
+    n = len(sids)
+
+    prefix_a = np.zeros(n, dtype="int")
+    prefix_b = np.zeros(n, dtype="int")
+    for i in range(n):
+        if i == 0:
+            prefix_a[i] = sids[i] == ra
+            prefix_b[i] = sids[i] == rb
+        else:
+            prefix_a[i] = prefix_a[i - 1] + (sids[i] == ra)
+            prefix_b[i] = prefix_b[i - 1] + (sids[i] == rb)
+    suffix_a = [0] * n
+    suffix_b = [0] * n
+    for i in range(n - 1, 0, -1):
+        suffix_a[i - 1] = suffix_a[i] + (sids[i] == ra)
+        suffix_b[i - 1] = suffix_b[i] + (sids[i] == rb)
+    ab = prefix_a + suffix_b
+    ba = prefix_b + suffix_a
+
+    if ab.max() > ba.max():
+        prefix, suffix = prefix_a, suffix_b
+        idx = np.argmax(ab)
+    else:
+        prefix, suffix = prefix_b, suffix_a
+        idx = np.argmax(ba)
+        ra, rb = rb, ra
+
+    count_ra = prefix[idx]
+    count_rb = suffix[idx]
+    if count_ra < KMER_THRESHOLD or count_rb < KMER_THRESHOLD:
+        logger.info(
+            "Breakpoint at `%s` has less than %d kmers. Skipped", read, KMER_THRESHOLD
+        )
+        return NOT_ENOUGH_KMERS
+    left = sb[idx].end
+    print(sb[idx])
+    if idx == n - 1:
+        logger.info("Breakpoint at the end of `%s`. Skipped", read)
+        return FAIL
+    right = sb[idx + 1].start
+    print(sb[idx + 1])
+    mid = (left + right) // 2
+    seq = fasta[read]
+    end = len(seq)
+    left_subread_id = f"{orig_read}|{ra}|0-{mid}"
+    left_subread = seq.seq[:mid]
+    right_subread_id = f"{orig_read}|{rb}|{mid}-{end}"
+    right_subread = seq.seq[mid:]
+    print(left_subread_id, right_subread_id)
+    new_fasta.write(f">{left_subread_id}\n{left_subread}\n")
+    new_fasta.write(f">{right_subread_id}\n{right_subread}\n")
+    return SUCCESS
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
