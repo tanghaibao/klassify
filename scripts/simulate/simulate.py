41db3003846962eb77f61f437cd86c35ca2ff725
Haibao Tang <haibao@amazon.com>
2025-08-20 20:43:52 +0000

    Add options

--- scripts/simulate/simulate.py
+++ scripts/simulate/simulate.py
@@ -14,8 +14,74 @@ Procedure:
 - Move results to the results directory
 """
 
+import argparse
+import os
+import os.path as op
+import random
+
+from dataclasses import dataclass
+from itertools import batched
+from random import sample
+from typing import List
+
+
+@dataclass
+class SimulatedConfig:
+    parents: List[str]
+    gametes: List[str]
+    seed: int
+
+
+def get_genomes(ref_dir: str):
+    """
+    Get a list of genome files from the reference directory.
+    """
+    genomes = [
+        op.basename(f).split(".", 1)[0]
+        for f in os.listdir(ref_dir)
+        if f.endswith(".fa")
+    ]
+    if len(genomes) < 8:
+        raise ValueError("Not enough genomes found in the reference directory.")
+    return genomes
+
+
+def admix(genomes: List[str], n: int, seed: int) -> SimulatedConfig:
+    """
+    Create mosaic genomes by randomly selecting pairs of genomes.
+    """
+    random.seed(seed)
+    parents = sample(genomes, n)
+    gametes = ["_".join(x) for x in batched(parents, 2)]
+    return SimulatedConfig(
+        parents,
+        gametes,
+        seed,
+    )
+
+
 def main():
-    pass
+    p = argparse.ArgumentParser(
+        description="Simulate mosaic genomes and run the klassify pipeline."
+    )
+    p.add_argument(
+        "--ref", default="ref", help="Directory containing reference genomes."
+    )
+    p.add_argument(
+        "--mosaics", default="mosaics", help="Directory containing reference genomes."
+    )
+    p.add_argument("--out", default="results", help="Directory to store the results.")
+    p.add_argument(
+        "-n", type=int, default=2, help="Number of genomes to use (2, 4, or 8)."
+    )
+    p.add_argument(
+        "--seed", type=int, default=42, help="Random seed for reproducibility."
+    )
+
+    args = p.parse_args()
+    genomes = get_genomes(args.ref)
+    res = admix(genomes, args.n, seed=args.seed)
+    print(res)
 
 
 if __name__ == "__main__":
