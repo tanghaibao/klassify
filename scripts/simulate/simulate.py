9fe7541e66d18fbea798a9b562b0f622478b45c4
Haibao Tang <haibao@amazon.com>
2025-08-20 22:04:41 +0000

    Generate input files

--- scripts/simulate/simulate.py
+++ scripts/simulate/simulate.py
@@ -24,6 +24,9 @@ from itertools import batched
 from random import sample
 from typing import List
 
+from jcvi.apps.base import logger, mkdir
+from jcvi.formats.base import FileMerger
+
 
 @dataclass
 class SimulatedConfig:
@@ -60,6 +63,33 @@ def admix(genomes: List[str], n: int, seed: int) -> SimulatedConfig:
     )
 
 
+def prepare_input(
+    config: SimulatedConfig, ref_dir: str, mosaics_dir: str, out_dir: str
+):
+    """
+    Prepare the input files for the klassify pipeline.
+    """
+    mkdir(out_dir)
+    parents_genomes = op.join(out_dir, "parents.genomes.fa")
+    parent_reads = op.join(out_dir, "parent_reads.fq.gz")
+    f1_reads = op.join(out_dir, "f1_reads.fq.gz")
+    parents = [op.join(ref_dir, f"{p}.fa") for p in config.parents]
+    # Concatenate parent genomes
+    FileMerger(parents, parents_genomes).merge()
+    logger.info("Parents genomes written to `%s`", parents_genomes)
+    # Concatenate parent reads
+    parent_reads_list = [f"{ref_dir}/{p}_0001.ccs.fastq.gz" for p in config.parents]
+    FileMerger(parent_reads_list, parent_reads).merge()
+    logger.info("Parent reads written to `%s`", parent_reads)
+    # Concatenate mosaic reads
+    f1_reads_list = [
+        f"{mosaics_dir}/{gamete}/{gamete}.mosaic_0001.ccs.fastq.gz"
+        for gamete in config.gametes
+    ]
+    FileMerger(f1_reads_list, f1_reads).merge()
+    logger.info("F1 reads written to `%s`", f1_reads)
+
+
 def main():
     p = argparse.ArgumentParser(
         description="Simulate mosaic genomes and run the klassify pipeline."
@@ -70,7 +100,7 @@ def main():
     p.add_argument(
         "--mosaics", default="mosaics", help="Directory containing reference genomes."
     )
-    p.add_argument("--out", default="results", help="Directory to store the results.")
+    p.add_argument("--out", default="out", help="Directory to store the results.")
     p.add_argument(
         "-n", type=int, default=2, help="Number of genomes to use (2, 4, or 8)."
     )
@@ -81,7 +111,11 @@ def main():
     args = p.parse_args()
     genomes = get_genomes(args.ref)
     res = admix(genomes, args.n, seed=args.seed)
-    print(res)
+    logger.info(res)
+
+    # Prepare input files for the klassify pipeline
+    prepare_input(res, args.ref, args.mosaics, args.out)
+    logger.info("Input files prepared in `%s`", args.out)
 
 
 if __name__ == "__main__":
